49e8f6271c67ce0926b2b0e6153826dfdb36bcb2
calebjakemossey <calebjakemossey@gmail.com>
2023-06-22 16:36:59 +0100

    delete test & remove exit code 0 on failure

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,1 @@
 import { render, screen } from "@testing-library/react";
-import Index from "../pages/index";
-import "@testing-library/jest-dom";
-
-describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Index />);
-
-    const heading = screen.getByRole("heading", {
-      name: /Your project is a g!/i,
-    });
-
-    expect(heading).toBeInTheDocument();
-  });
-});
