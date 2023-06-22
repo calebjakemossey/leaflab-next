a06a032e1c1cf07df215d00f1da3408465c5cb21
calebjakemossey <calebjakemossey@gmail.com>
2023-06-22 16:39:44 +0100

    re add test, make requirement 100%

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,1 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import Index from "../pages/index";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Index />);
+
+    const heading = screen.getByRole("heading", {
+      name: /Your project is a go!/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
