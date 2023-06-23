e60414e946216fcf51e37213f1686e92cb8d12c3
calebjakemossey <calebjakemossey@gmail.com>
2023-06-23 16:57:32 +0100

    Stuff!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,17 @@
 import { AppProps } from "next/app";
-import { MantineProvider } from "@mantine/core";
-import { mantineTheme } from "../mantine-theme";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "@/styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = React.useState(() => new QueryClient());
   return (
     <>
-      <MantineProvider withGlobalStyles withNormalizeCSS theme={mantineTheme}>
-        <QueryClientProvider client={queryClient}>
-          <ReactQueryDevtools />
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </MantineProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-import { createGetInitialProps } from "@mantine/next";
-
-const getInitialProps = createGetInitialProps();
 
 export default class Document extends NextDocument {
-  static getInitialProps = getInitialProps;
-
   render() {
     return (
       <Html lang="en">
